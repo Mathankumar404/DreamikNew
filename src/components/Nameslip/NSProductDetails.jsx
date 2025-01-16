43f9664fac61394ab75228149705e3f1c5388fb4
Mathankumar404 <mathankvm123@gmail.com>
2025-01-16 10:53:08 +0530

    Add files via upload

---
+++ src/components/Nameslip/NSProductDetails.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+
+const NSProductDetails = ({ navigateTo }) => {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = '/data.json';
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        localStorage.setItem('data', JSON.stringify(data));
+        return data;
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+      }
+    };
+
+    const loadProductDetails = async () => {
+      const data = await fetchData();
+      const key = localStorage.getItem('keyid');
+      if (key && data[key]) {
+        setProduct(data[key]);
+        document.title = data[key].name;
+      }
+    };
+
+    loadProductDetails();
+  }, []);
+
+  const handlePersonalizeAndAddToCart = (id) => {
+    
+      localStorage.setItem('keyid', id);
+      navigateTo('NSPersonalize');  // Navigate to NSPersonalize view
+    };
+  
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section id="prodetails" className="section-p1">
+      <div className="single-pro-image">
+        <img src={product.source} alt={product.name} width="100%" id="MainImg" />
+      </div>
+      <div className="single-pro-details" id="details">
+        <h6>Home / Product</h6>
+        <h4>{product.name}</h4>
+        <h2>Rs. {product.price}</h2>
+
+        <h4>Product Details</h4>
+        <span>{product.name}</span>
+        <br />
+        <span>{product.props.join(', ')}</span>
+        <br />
+        <button className="P-btn" id="targetbtn" onClick={() => handlePersonalizeAndAddToCart(product.id)} >
+          Personalize and Add To Cart
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default NSProductDetails;
