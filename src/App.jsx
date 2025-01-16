0c468945551986effaaabefd2eab0a8cf395fb7b
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-01-16 15:02:35 +0530

    updated by sundar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
-import './App.css';
-import './index.css';
-import "./style.css";
+// import './App.css';
+// import './index.css';
+// import "./style.css";
+// import "./components/order/Order.css"
 import Navbar from './components/Nav';
 import Footer from './components/Footer';
 import ProductList from './components/ProductList';
 import Newsletter from './components/Newsletter';
 import Advertisement from './components/Advertisement';
-import CutOutNameSlip from './components/cutoutnameslip/CutOutNameSlip'; 
+import NameSlips from './components/nameslip/NameSlips';
+import Products from './components/nameslip/Products';
+import CutOutNameSlip from './components/cutoutnameslip/CutoutNameslip'; 
 import ProductDetails from './components/cutoutnameslip/ProductDetails'; 
 import Order from './components/order/Order'; // Assuming you will create this component
-import Nameslip from './components/Nameslip/Nameslip';
-import Bagtag from './components/Bagtag/Bagtag';
-import NSProductDetails from './components/Nameslip/NSProductDetails';
-import NSPersonalize from './components/Personalize/NSPersonalize';
+import Template from './components/cutoutnameslip/Template';
+import BulkOrder from './components/bulkorder/BulkOrder';
+import NSPersonalize from './components/nameslip/NSPersonalize';
+import CustamizableBagTage from './components/bagtag/CustamizableBagTage'
+
 function App() {
   const [cartCount, setCartCount] = useState(0);
   const [showResellerLogin, setShowResellerLogin] = useState(false);
@@ -29,12 +33,14 @@ function App() {
       <Navbar cartCount={cartCount} navigateTo={navigateTo} />
       <Advertisement />
       {currentView === 'ProductList' && <ProductList navigateTo={navigateTo} />}
-      {currentView === 'CutOutNameSlip' && <CutOutNameSlip navigateTo={navigateTo} />}
+      {currentView === 'NameSlips' && <NameSlips navigateTo={navigateTo}/>}
+      {currentView === 'Products' && <Products navigateTo={navigateTo} />}
+      {currentView === 'NSPersonalize' && <NSPersonalize navigateTo={navigateTo}/>}
+      {currentView === 'CutoutNameslip' && <CutOutNameSlip navigateTo={navigateTo} />}
       {currentView === 'ProductDetails' && <ProductDetails navigateTo={navigateTo} />}
-      {currentView === 'Nameslip' &&   <Nameslip navigateTo={navigateTo} />}
-      {currentView === 'NSProductDetails' &&   <NSProductDetails navigateTo={navigateTo} />}
-      {currentView === 'Bagtag' &&   <Bagtag navigateTo={navigateTo} />}
-      {currentView === 'NSPersonalize' &&   <NSPersonalize navigateTo={navigateTo} />}
+      {currentView === 'Template' && <Template navigateTo={navigateTo}/>}
+      {currentView == 'CustamizableBagTage' && <CustamizableBagTage navigateTo={navigateTo}/>}
+      {currentView === 'BulkOrder' && <BulkOrder navigateTo={navigateTo}/>}
       {currentView === 'Order' && <Order />} {/* Add Order view here */}
       <Newsletter />
       <Footer />
