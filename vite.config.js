678496d4948bf80c0dd87098db3ef61625406dc3
Mathankumar404 <mathankvm123@gmail.com>
2025-01-18 20:17:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
- 
-  build: {
-   rollupOptions: {
-      external: ['jszip'],  // This tells Vite not to bundle jszip and to expect it externally
-    },
-    outDir: 'build',  // Optional: change output directory from 'dist' to 'build'
-    cssMinify: true,  // Optional: ensure CSS is minified
-  },
-   
   plugins: [react()],
 })
