f1e36fe87de1dfd62a416c192723eae8fd45d6dc
Mathankumar404 <mathankvm123@gmail.com>
2025-01-18 20:03:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+ 
+  build: {
+    outDir: 'build',  // Optional: change output directory from 'dist' to 'build'
+    cssMinify: true,  // Optional: ensure CSS is minified
+  },
+   
   plugins: [react()],
 })
